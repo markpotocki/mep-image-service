a7b163ca952c45fc4db28790d202f8993b2247ee
markpotocki <mark-potocki@kmptechnologies.com>
2018-06-10 22:47:50 -0500

    Add find image by userId method

--- src/main/java/mep/mvcsocial/imageservice/domain/ImageRepo.java
+++ src/main/java/mep/mvcsocial/imageservice/domain/ImageRepo.java
@@ -1,6 +1,10 @@
 package mep.mvcsocial.imageservice.domain;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ImageRepo extends MongoRepository<Image, String> {
+    Page<Image> findAllByUserId(String userId, Pageable page);
+
 }
--- src/test/java/mep/mvcsocial/imageservice/service/ImageServiceMongoTest.java
+++ src/test/java/mep/mvcsocial/imageservice/service/ImageServiceMongoTest.java
@@ -1,8 +1,44 @@
 package mep.mvcsocial.imageservice.service;
 
 
+import mep.mvcsocial.imageservice.domain.Image;
+import mep.mvcsocial.imageservice.domain.ImageRepo;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.Instant;
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 @DataMongoTest
+@RunWith(SpringRunner.class)
 public class ImageServiceMongoTest {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+    @Autowired
+    private ImageRepo imageRepo;
+
+
+    @Test
+    public void findImageByUserId() {
+        // setup
+        Image image = new Image("foo", "test", "file", Instant.now().toEpochMilli(), 0);
+        mongoTemplate.save(image);
+
+        Page<Image> actual = imageRepo.findAllByUserId("test", PageRequest.of(0, 10));
+        List<Image> actualAsList = actual.getContent();
+
+        assertTrue(actualAsList.contains(image)); // should contian saved image
+        assertTrue(actualAsList.size() == 1); // should contain 1 element
+    }
 }
