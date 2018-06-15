d0617874ab1fe80b2415fce27d0895912dd7ce4f
markpotocki <mark-potocki@kmptechnologies.com>
2018-06-15 00:26:13 -0500

    Adds web security annotation
    it was not configuring correctly without it

--- src/test/java/mep/mvcsocial/imageservice/ImageControllerTest.java
+++ src/test/java/mep/mvcsocial/imageservice/ImageControllerTest.java
@@ -7,6 +7,7 @@ import mep.mvcsocial.imageservice.service.ImageService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.core.io.FileSystemResource;
@@ -16,6 +17,9 @@ import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -24,6 +28,7 @@ import java.util.List;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -49,7 +54,7 @@ public class ImageControllerTest {
         // when imageService return testImage
         when(imageService.getImageByUserId("user")).thenReturn(List.of(testImage));
 
-        mockMvc.perform(get("/api/services/images?user=user"))
+        mockMvc.perform(get("/api/services/images?user=user").header("Authorization", "Bearer " + getAccessToken("admin", "admin")))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(content().json(objectMapper.writeValueAsString(List.of(testImage))));
@@ -91,5 +96,27 @@ public class ImageControllerTest {
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
                 //.andReturn()
                 //.andExpect(content().json(objectMapper.writeValueAsString(testImage)));
+    }
+
+    private String getAccessToken(String username, String password) throws Exception {
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "password");
+        params.add("client_id", "d8110ba3-46bb-4eec-a92f-4c1a1b750654");
+        params.add("username", username);
+        params.add("password", password);
+
+        ResultActions result = mockMvc.perform(
+                post("/oauth/token")
+                        .params(params)
+                        .with(httpBasic("d8110ba3-46bb-4eec-a92f-4c1a1b750654","a31ced2d-86b2-4f6c-a2ce-10c0f5d08d8d"))
+                        .accept("application/json;charset=UTF-8"))
+                        .andExpect(status().isOk())
+                        .andExpect(content().contentType("application/json;charset=UTF-8"));
+
+        String json = result.andReturn().getResponse().getContentAsString();
+
+        JacksonJsonParser parser = new JacksonJsonParser();
+        return parser.parseMap(json).get("access_token").toString();
     }
 }
